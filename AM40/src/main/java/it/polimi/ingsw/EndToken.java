c8fc2f0c4a11702ded4d87344a3bc1169be06c6a
Francesco Di Giore <francesco.digiore@mail.polimi.it>
2023-03-17 20:40:33 +0100

    Board, EndToken and Personal Goal done

---
+++ AM40/src/main/java/it/polimi/ingsw/EndToken.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw;
+
+public class EndToken {
+    private boolean end;
+    private Player player;
+
+    public EndToken() {
+        end = false;
+        player = null;
+    }
+    public boolean check (Player p) {
+        return player.getBookshelf().isFull();
+    }
+    public void updateScore (Player p) {
+        if (player == null && !end && check(p)) {
+            p.addCurrentScore(1);
+        }
+    }
+
+    public boolean isEnd() {
+        return end;
+    }
+
+    public void setEnd(boolean end) {
+        this.end = end;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
---
+++ AM40/src/main/java/it/polimi/ingsw/TileColor.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw;
+
+public enum TileColor {
+    GREEN, WHITE, YELLOW, BLUE, CYAN, VIOLET;
+}
