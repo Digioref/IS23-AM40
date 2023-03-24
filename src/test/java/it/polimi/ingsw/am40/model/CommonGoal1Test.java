e8636e0529adb5ec1c49b21366f17c2f8b7688c2
filippobertossi <filippomaria.bertossi@mail.polimi.it>
2023-03-24 10:01:11 +0100

    Optimized CommonGoal Test

--- src/test/java/it/polimi/ingsw/am40/model/CommonGoal1Test.java
+++ src/test/java/it/polimi/ingsw/am40/model/CommonGoal1Test.java
@@ -2,48 +2,40 @@ package it.polimi.ingsw.am40.model;
 
 import it.polimi.ingsw.am40.model.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*; // what ???
 
 public class CommonGoal1Test {
 
     @Test
-    public void Test() {
+    public void Test() { // GREEN, WHITE, YELLOW, BLUE, CYAN, VIOLET
 
-        Bookshelf b = new Bookshelf();
-        Tile t1 = new Tile(TileColor.WHITE, TileType.CATS); // colors: GREEN, WHITE, YELLOW, BLUE, CYAN, VIOLET, NOCOLOR;
-        Tile t2 = new Tile(TileColor.GREEN, TileType.CATS);
+        Bookshelf bookshelf = new Bookshelf();
+        Tile g = new Tile(TileColor.GREEN, TileType.CATS); // colors: GREEN, WHITE, YELLOW, BLUE, CYAN, VIOLET, NOCOLOR;
+        Tile w = new Tile(TileColor.WHITE, TileType.CATS);
+        Tile y = new Tile(TileColor.YELLOW, TileType.CATS);
+        Tile b = new Tile(TileColor.BLUE, TileType.CATS);
+        Tile c = new Tile(TileColor.CYAN, TileType.CATS);
+        Tile v = new Tile(TileColor.VIOLET, TileType.CATS);
 
-        b.addTile(t2,0);
-        b.addTile(t1,1);
-        b.addTile(t2,2);
-        b.addTile(t1,3);
-        b.addTile(t2,4);
+        ArrayList<Tile> row5 = new ArrayList<>(List.of(g,g,g,g,g));
+        ArrayList<Tile> row4 = new ArrayList<>(List.of(g,g,g,g,g));
+        ArrayList<Tile> row3 = new ArrayList<>(List.of(g,g,g,g,g));
+        ArrayList<Tile> row2 = new ArrayList<>(List.of(g,g,g,g,g));
+        ArrayList<Tile> row1 = new ArrayList<>(List.of(g,g,g,g,g));
+        ArrayList<Tile> row0 = new ArrayList<>(List.of(g,g,g,g,g));
 
-        b.addTile(t1,0);
-        b.addTile(t1,1);
-        b.addTile(t1,2);
-        b.addTile(t1,3);
-        b.addTile(t1,4);
+        ArrayList<ArrayList<Tile>> structure = new ArrayList<>(List.of(row0, row1, row2, row3, row4, row5));
 
-        b.addTile(t2,0);
-        b.addTile(t2,1);
-        b.addTile(t1,2);
-        b.addTile(t2,3);
-        b.addTile(t2,4);
+        for (int i = 0; i < structure.size(); i++) {
+            for (int j = 0; j < structure.get(i).size(); j++) {
+                bookshelf.addTile(structure.get(i).get(j),j);
+            }
+        }
 
-        b.addTile(t2,0);
-        b.addTile(t2,1);
-        b.addTile(t1,2);
-        b.addTile(t2,3);
-        b.addTile(t2,4);
-
-        b.addTile(t2,0);
-        b.addTile(t2,1);
-        b.addTile(t1,2);
-        b.addTile(t2,3);
-        b.addTile(t2,4);
-
-        b.addTile(t1,2);
 /*
         b.addTile(t1,0);
         b.addTile(t1,1);
@@ -76,8 +68,8 @@ public class CommonGoal1Test {
 
         //CommonGoal1 c1 = new CommonGoal1(2);
         CommonGoal3 c1 = new CommonGoal3(2);
-        assertEquals(8, c1.check(b));
-        assertEquals(4, c1.check(b));
+        assertEquals(8, c1.check(bookshelf));
+        assertEquals(4, c1.check(bookshelf));
 
 
 
