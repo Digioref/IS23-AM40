339a48949e7e9fa5c9978b0de469a8a90c5da19c
filippobertossi <filippomaria.bertossi@mail.polimi.it>
2023-03-30 11:12:48 +0200

    New TestEmpty to test every case of the bookshelf

--- src/test/java/it/polimi/ingsw/am40/Model/CommonGoalTest2.java
+++ src/test/java/it/polimi/ingsw/am40/Model/CommonGoalTest2.java
@@ -116,45 +116,96 @@ public class CommonGoalTest2 {
     }
 
     @Test
-    public void Test2() { // we have to do some test without the json to test the empty cells
+    public void TestEmpty() {
 
-        Bookshelf bookshelf = new Bookshelf();
-        Tile g = new Tile(TileColor.GREEN, TileType.CATS); // colors: GREEN, WHITE, YELLOW, BLUE, CYAN, VIOLET, NOCOLOR;
-        Tile w = new Tile(TileColor.WHITE, TileType.CATS);
-        Tile y = new Tile(TileColor.YELLOW, TileType.CATS);
-        Tile b = new Tile(TileColor.BLUE, TileType.CATS);
-        Tile c = new Tile(TileColor.CYAN, TileType.CATS);
-        Tile v = new Tile(TileColor.VIOLET, TileType.CATS);
+        JSONParser jsonParser = new JSONParser();
+        FileReader reader;
 
-        ArrayList<Tile> row5 = new ArrayList<>(List.of(g, g, g, g, g));
-        ArrayList<Tile> row4 = new ArrayList<>(List.of(g, g, g, g, g));
-        ArrayList<Tile> row3 = new ArrayList<>(List.of(g, g, g, g, g));
-        ArrayList<Tile> row2 = new ArrayList<>(List.of(g, g, g, g, g));
-        ArrayList<Tile> row1 = new ArrayList<>(List.of(g, g, g, g, g));
-        ArrayList<Tile> row0 = new ArrayList<>(List.of(g, g, g, g, g));
+        try {
+            reader = new FileReader("CommonGoalsEmpty.json");
+            JSONObject commonGoals = (JSONObject) jsonParser.parse(reader);
 
-        ArrayList<ArrayList<Tile>> structure = new ArrayList<>(List.of(row0, row1, row2, row3, row4, row5));
+            JSONArray array = (JSONArray) commonGoals.get("CommonGoals");
 
-        for (ArrayList<Tile> tiles : structure) {
-            for (int j = 0; j < tiles.size(); j++) {
-                bookshelf.addTile(tiles.get(j), j);
+            for (int i = 0; i < array.size(); i++) {
+
+                ArrayList<Tile> row;
+                ArrayList<ArrayList<Tile>> book = new ArrayList<>(6);
+                ArrayList<Integer> asserts = new ArrayList<>(7);
+
+                JSONObject o = (JSONObject) array.get(i);
+
+                JSONArray obj1 = (JSONArray) o.get("bookshelf");
+                JSONArray obj2 = (JSONArray) o.get("asserts");
+
+                Bookshelf bookshelf = new Bookshelf();
+
+                for (int k = 0; k < obj1.size(); k++) {
+                    JSONObject col = (JSONObject) obj1.get(k);
+                    JSONArray column = (JSONArray) col.get("col");
+
+                    for (Object value : column) {
+                        switch (value.toString()) {
+                            case "Y" -> {
+                                bookshelf.addTile(new Tile(TileColor.YELLOW, TileType.CATS), k);
+                            }
+                            case "W" -> {
+                                bookshelf.addTile(new Tile(TileColor.WHITE, TileType.CATS), k);
+                            }
+                            case "B" -> {
+                                bookshelf.addTile(new Tile(TileColor.BLUE, TileType.CATS), k);
+                            }
+                            case "G" -> {
+                                bookshelf.addTile(new Tile(TileColor.GREEN, TileType.CATS), k);
+                            }
+                            case "C" -> {
+                                bookshelf.addTile(new Tile(TileColor.CYAN, TileType.CATS), k);
+                            }
+                            case "V" -> {
+                                bookshelf.addTile(new Tile(TileColor.VIOLET, TileType.CATS), k);
+                            }
+                        }
+                    }
+                }
+
+                CommonGoal1 c1 = new CommonGoal1(2);
+                CommonGoal2 c2 = new CommonGoal2(2);
+                CommonGoal3 c3 = new CommonGoal3(2);
+                CommonGoal4 c4 = new CommonGoal4(2);
+                CommonGoal5 c5 = new CommonGoal5(2);
+                CommonGoal6 c6 = new CommonGoal6(2);
+                CommonGoal7 c7 = new CommonGoal7(2);
+                CommonGoal8 c8 = new CommonGoal8(2);
+                CommonGoal9 c9 = new CommonGoal9(2);
+                CommonGoal10 c10 = new CommonGoal10(2);
+                CommonGoal11 c11 = new CommonGoal11(2);
+                CommonGoal12 c12 = new CommonGoal12(2);
+
+                System.out.println("i: " + i);
+                System.out.println(obj2.size());
+
+                for (Object value : obj2) {
+                    asserts.add(Integer.parseInt(value.toString()));
+                }
+
+
+                assertEquals((int) asserts.get(0), c1.check(bookshelf));
+                assertEquals((int) asserts.get(1), c2.check(bookshelf));
+                assertEquals((int) asserts.get(2), c3.check(bookshelf));
+                assertEquals((int) asserts.get(3), c4.check(bookshelf));
+                assertEquals((int) asserts.get(4), c5.check(bookshelf));
+                assertEquals((int) asserts.get(5), c6.check(bookshelf));
+                assertEquals((int) asserts.get(6), c7.check(bookshelf));
+                assertEquals((int) asserts.get(7), c8.check(bookshelf));
+                assertEquals((int) asserts.get(8), c9.check(bookshelf));
+                assertEquals((int) asserts.get(9), c10.check(bookshelf));
+                assertEquals((int) asserts.get(10), c11.check(bookshelf));
+                assertEquals((int) asserts.get(11), c12.check(bookshelf));
+
             }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
         }
-
-        CommonGoal1 c1 = new CommonGoal1(2);
-        CommonGoal2 c2 = new CommonGoal2(2);
-        CommonGoal3 c3 = new CommonGoal3(2);
-        CommonGoal4 c4 = new CommonGoal4(2);
-        CommonGoal5 c5 = new CommonGoal5(2);
-        CommonGoal6 c6 = new CommonGoal6(2);
-
-        assertEquals(8, c1.check(bookshelf));
-        assertEquals(0, c2.check(bookshelf));
-        assertEquals(8, c3.check(bookshelf));
-        assertEquals(8, c4.check(bookshelf));
-        assertEquals(8, c5.check(bookshelf));
-        assertEquals(0, c6.check(bookshelf));
-
-
     }
 }
