48934f782b068779f19b91b1346982f19ae8e1aa
MarcoBrambillasca <marcoandrea.brambillasca@mail.polimi.it>
2023-06-21 16:43:54 +0200

    seconda parte javadoc e pulizia codice

---
+++ src/test/java/it/polimi/ingsw/am40/Controller/GameControllerTest.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.am40.Controller;
+
+import it.polimi.ingsw.am40.Model.Game;
+import it.polimi.ingsw.am40.Model.Player;
+import it.polimi.ingsw.am40.Network.*;
+import it.polimi.ingsw.am40.Network.RMI.RMIServer;
+import org.junit.jupiter.api.Test;
+
+import java.net.Socket;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerTest {
+
+    @Test
+    void selectTile() throws RemoteException {
+    }
+
+    @Test
+    void pickTiles() {
+    }
+
+    @Test
+    void notConfirmSelection() {
+    }
+
+    @Test
+    void order() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void chat() {
+    }
+
+    @Test
+    void getChat() {
+    }
+
+    @Test
+    void setController() {
+    }
+
+    @Test
+    void disconnectPlayer() {
+    }
+
+    @Test
+    void getGame() {
+    }
+
+    @Test
+    void getController() {
+    }
+
+    @Test
+    void reconnect() {
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/am40/Controller/LobbyTest.java
@@ -0,0 +1,60 @@
+package it.polimi.ingsw.am40.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LobbyTest {
+
+    @Test
+    void removeFromQueue() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void findGame() {
+    }
+
+    @Test
+    void addQueue() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getNicknameInGame() {
+    }
+
+    @Test
+    void getActivePlayers() {
+    }
+
+    @Test
+    void setNumPlayers() {
+    }
+
+    @Test
+    void getQueue() {
+    }
+
+    @Test
+    void addNickname() {
+    }
+
+    @Test
+    void removeQuit() {
+    }
+
+    @Test
+    void getGames() {
+    }
+
+    @Test
+    void closeGame() {
+    }
+}
